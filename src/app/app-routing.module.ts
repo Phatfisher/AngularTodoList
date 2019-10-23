7dd1d630f8b6f7770a6c26827db261d031e6c429
Ma Vang <ma.vang@umontana.edu>
2019-10-23 11:20:10 -0600

    Adding in About page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TodosComponent} from './components/todos/todos.component';
 
+const routes: Routes = [
+  {path: '', component:TodosComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
